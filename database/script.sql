fee3fe6e22d44086980c960fa2c1a6bbedc9c97a
JOÃO VITOR BLESA SILVA <25132585@CFP127ALUNO.LOCAL>
2025-12-03 13:14:13 -0300

    atualizando o codigo sql

--- database/script.sql
+++ database/script.sql
@@ -122,8 +122,27 @@ create table tbl_item_pedido (
     foreign key (id_ingresso) references tbl_ingresso (id_ingresso)
 );
 
+select * from tbl_assunto;
+select * from tbl_categoria;
+select * from tbl_cliente;
+select * from tbl_cliente_evento;
+select * from tbl_endereco;
+select * from tbl_estado;
+select * from tbl_evento;
+select * from tbl_genero;
+select * from tbl_ingresso;
+select * from tbl_item_pedido;
+select * from tbl_organizador;
+select * from tbl_pedido;
 
-
+create table tbl_organizador_evento (
+id_organizador_evento int primary key auto_increment,
+data_inscricao datetime not null,
+id_organizador int not null,
+id_evento int not null,
+foreign key (id_organizador) references tbl_organizador (id_organizador),
+foreign key (id_evento) references tbl_evento (id_evento)
+);
 
 
 -- inserts
