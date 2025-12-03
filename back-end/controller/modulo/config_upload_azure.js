064022bc8923be206c953813a18936b663615ae1
VITOR MIGUEL RODRIGUES CEZARIO <25132565@CFP127ALUNO.LOCAL>
2025-12-03 15:11:10 -0300

    Padronizado as mensagens de error e colocado arquivo para link com Azure

---
+++ back-end/controller/modulo/config_upload_azure.js
@@ -0,0 +1,14 @@
+/***************************************************************************************
+ * Objetivo: Arquivo de configuração do UPLOAD de arquivos na Azure
+ * Data: 28/10/2025
+ * Autor: Marcel
+ * Versão: 1.0
+ ***************************************************************************************/
+
+//const TOKEN     = 'sp=racwd&st=2025-10-28T11:26:48Z&se=2025-10-28T19:41:48Z&sv=2024-11-04&sr=c&sig=qt443p615EwW90OXSPD3KixCQZo8DKD0lxjY05LibY4%3D'
+const TOKEN     = 'sp=rcw&st=2025-11-24T19:47:02Z&se=2025-11-24T21:02:02Z&sv=2024-11-04&sr=c&sig=7uEbrCr4ioIp9d4mdrnarsR1aaLOqRkPWMpJ2Zlm2uo%3D'
+const ACCOUNT   = 'uploadteste'
+const CONTAINER = 'uploadteste'
+
+
+module.exports = {TOKEN, ACCOUNT, CONTAINER}
