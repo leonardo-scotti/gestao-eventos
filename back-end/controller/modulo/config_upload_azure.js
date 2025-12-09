775776bf73a603852ba35a392a73b63d1983dc23
Vfy-gif <vitormiguel.school@gmail.com>
2025-12-08 22:47:59 -0300

    Token definitivo

--- back-end/controller/modulo/config_upload_azure.js
+++ back-end/controller/modulo/config_upload_azure.js
@@ -6,7 +6,7 @@
  ***************************************************************************************/
 
 //const TOKEN     = 'sp=racwd&st=2025-10-28T11:26:48Z&se=2025-10-28T19:41:48Z&sv=2024-11-04&sr=c&sig=qt443p615EwW90OXSPD3KixCQZo8DKD0lxjY05LibY4%3D'
-const TOKEN     = 'sp=rcw&st=2025-12-07T02:48:01Z&se=2025-12-13T00:03:01Z&sv=2024-11-04&sr=c&sig=VYlugixNtQQgVyuZ3ebsbKo4sWOEdWJLieerPifR4q8%3D'
+const TOKEN     = 'sp=rcw&st=2025-12-09T01:26:56Z&se=2025-12-27T09:41:56Z&sv=2024-11-04&sr=c&sig=M5dGnIs9NfGJTLZW0cH2yaGVny0bIlfX6xDIDkOrtwM%3D'
 const ACCOUNT   = 'nuvemsenai'
 const CONTAINER = 'uploudimagens'
 
