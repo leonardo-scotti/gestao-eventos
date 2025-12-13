ea5f787937ab9c82c18c85562ca68f0ac0983269
Leonardo-Scotti <scottileonardo1@gmail.com>
2025-12-13 14:29:02 -0300

    Ajustando script sql

--- database/script.sql
+++ database/script.sql
@@ -215,7 +215,9 @@ BEGIN
     )
     WHERE p.id_pedido = NEW.id_pedido;
 END$$
+DELIMITER ;
 
+DELIMITER $$
 -- 4.2. Calcular total do pedido ao ATUALIZAR item
 CREATE TRIGGER trg_AU_item_pedido_calcular_total_pedido
 AFTER UPDATE ON tbl_item_pedido
@@ -230,7 +232,9 @@ BEGIN
     )
     WHERE p.id_pedido = NEW.id_pedido;
 END$$
+DELIMITER ;
 
+DELIMITER $$
 -- 4.3. Calcular total do pedido ao DELETAR item
 CREATE TRIGGER trg_AD_item_pedido_calcular_total_pedido
 AFTER DELETE ON tbl_item_pedido
@@ -245,7 +249,9 @@ BEGIN
     )
     WHERE p.id_pedido = OLD.id_pedido;
 END$$
+DELIMITER ;
 
+DELIMITER $$
 -- 4.4. Setar valores padrão para Pedido
 CREATE TRIGGER trg_BI_pedido_setar_valores_padrao
 BEFORE INSERT ON tbl_pedido
@@ -259,7 +265,9 @@ BEGIN
         SET NEW.status_pedido = 'EM_ANDAMENTO';
     END IF;
 END$$
+DELIMITER ;
 
+DELIMITER $$
 -- 4.5. Validar estoque ANTES de inserir item
 CREATE TRIGGER trg_BI_item_pedido_validar_quantidade_estoque
 BEFORE INSERT ON tbl_item_pedido
@@ -285,7 +293,9 @@ BEGIN
         SET MESSAGE_TEXT = 'Estoque insuficiente para a quantidade solicitada.';
     END IF;
 END$$
+DELIMITER ;
 
+DELIMITER $$
 -- 4.6. Atualizar contagem de estoque APÓS inserir item
 CREATE TRIGGER trg_AI_item_pedido_atualizar_estoque
 AFTER INSERT ON tbl_item_pedido
@@ -296,7 +306,9 @@ BEGIN
     SET e.quantidade_ingresso_comprado = e.quantidade_ingresso_comprado + NEW.quantidade
     WHERE i.id_ingresso = NEW.id_ingresso;
 END$$
+DELIMITER ;
 
+DELIMITER $$
 -- 4.7. Devolver estoque se pedido for CANCELADO
 CREATE TRIGGER trg_AU_pedido_cancelado_devolver_estoque
 AFTER UPDATE ON tbl_pedido
@@ -310,7 +322,9 @@ BEGIN
         WHERE ip.id_pedido = NEW.id_pedido;
     END IF;
 END$$
+DELIMITER ;
 
+DELIMITER $$
 -- 4.8. Validar data de início e fim do evento
 CREATE TRIGGER trg_BI_evento_validar_data_fim
 BEFORE INSERT ON tbl_evento
@@ -321,7 +335,9 @@ BEGIN
         SET MESSAGE_TEXT = 'A data/hora de término do evento deve ser posterior à data/hora de início.';
     END IF;
 END$$
+DELIMITER ;
 
+DELIMITER $$
 -- 4.9. Validações de Cliente (Nascimento e Documento)
 CREATE TRIGGER trg_BI_cliente_validar_data_nascimento
 BEFORE INSERT ON tbl_cliente
